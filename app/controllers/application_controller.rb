14faf4cfc9fe7ac75d0543f5e99626976a37929e
Paulo Santos <sungonku@gmail.com>
2014-09-02 11:38:18 +0000

    fix application controller again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,15 +11,15 @@ class ApplicationController < ActionController::Base
     redirect_to root_url, :alert => exception.message
   end
   
-  if Rails.env.production?
-    unless Rails.application.config.consider_all_requests_local
+  # if Rails.env.production?
+    # unless Rails.application.config.consider_all_requests_local
       rescue_from Exception, with: :render_500
       rescue_from ActionController::RoutingError, with: :render_404
       rescue_from ActionController::UnknownController, with: :render_404
       # rescue_from ActionController::UnknownAction, with: :render_404
       rescue_from ActiveRecord::RecordNotFound, with: :render_404
-    end
-  end
+    # end
+  # end
   
   protected
 
